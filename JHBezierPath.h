34bd213e225304026dfa46e9aaaf5f61a39f79aa
Jerry Zhang <jerry_zhang@mtelnet.com>
2019-10-11 14:54:09 +0800

    no message

---
+++ JHBezierPath.h
@@ -0,0 +1,17 @@
+//
+//  JHBezierPath.h
+//  AFTest
+//
+//  Created by zxj on 2019/10/11.
+//  Copyright © 2019 zxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHBezierPath : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JHBezierPath.m
@@ -0,0 +1,32 @@
+//
+//  JHBezierPath.m
+//  AFTest
+//
+//  Created by zxj on 2019/10/11.
+//  Copyright © 2019 zxj. All rights reserved.
+//
+
+#import "JHBezierPath.h"
+
+@interface JHBezierPath ()
+
+@end
+
+@implementation JHBezierPath
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
